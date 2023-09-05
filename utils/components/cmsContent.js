b093fd05679ac62a4b476b4674ba6a9708d08d7e
Gaëtan Jestin <gaetanjestin@outlook.com>
2023-09-05 13:56:23 +0200

    feat(content): images for cms table section

--- utils/components/cmsContent.js
+++ utils/components/cmsContent.js
@@ -4,35 +4,35 @@ const cmsContent = {
 		title: 'Welcome home 🫶!',
 		description:
 			'We offer you a customizable website, in the colors of your organization and en effective information architecture. Webpage, blog, contact, all your pages are freely editable with a library of blocks. Configure the SEO of your web pages at the same time.',
-		image: '/images/placeholder.png'
+		image: '/images/table/cms/home-en.jpg'
 	},
 	item2: {
 		btnTitle: 'User rights',
 		title: 'Control access 🔒',
 		description:
 			'Manage user rights in the backoffice, secure connections and trace who consulted a document and on what date. Ability to create groups of rights.',
-		image: '/images/placeholder.png'
+		image: '/images/table/cms/users-en.jpg'
 	},
 	item3: {
 		btnTitle: 'Job center',
 		title: 'Recruit quickly 🙋',
 		description:
 			'Publish and manage your job offers, centralize your applications in your extranet, involve your teams in the process.',
-		image: '/images/placeholder.png'
+		image: '/images/table/cms/jobs-en.jpg'
 	},
 	item4: {
 		btnTitle: 'Events',
 		title: 'Let the festivities begin! 🎉',
 		description:
 			'Create presentation pages and registration forms for all your events, free or paid. Manage the volumes of subscribers, send reminders before D-Day to ensure a maximum of presents.',
-		image: '/images/placeholder.png'
+		image: '/images/table/cms/events-en.jpg'
 	},
 	item5: {
 		btnTitle: 'Q&A',
 		title: 'Save time! ⌛',
 		description:
 			'Build a library of frequently asked questions for your community with filtered access to your key resources.',
-		image: '/images/placeholder.png'
+		image: '/images/table/cms/qna-en.jpg'
 	}
 }
 
