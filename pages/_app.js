1dc64d2dfa3b796413b6741846b0de30c79f98aa
Gaëtan Jestin <gaetanjestin@outlook.com>
2023-09-08 09:35:53 +0200

    feat(3D): added loader to website

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,11 @@ import '../styles/index.scss'
 import Head from 'next/head'
 import Layout from '../components/layout'
 import { GlobalCanvas } from '@14islands/r3f-scroll-rig'
-import { Environment, Lightformer } from '@react-three/drei'
+import { Environment, Lightformer, Loader } from '@react-three/drei'
 import * as THREE from 'three'
 import { Perf } from 'r3f-perf'
+
+import { Suspense } from 'react'
 
 export default function App({ Component, pageProps }) {
 	return (
@@ -21,21 +23,31 @@ export default function App({ Component, pageProps }) {
 						gl.toneMapping = THREE.NoToneMapping
 					}}
 				>
-					<color attach='background' args={['#FBFBFD']} />
+					<Suspense fallback={null}>
+						<color attach='background' args={['#FBFBFD']} />
 
-					<Environment resolution={32}>
-						<group rotation={[-Math.PI / 4, -0.3, 0]}>
-							<Lightformer intensity={20} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={[10, 10, 1]} />
-							<Lightformer intensity={2} rotation-y={Math.PI / 2} position={[-5, 1, -1]} scale={[10, 2, 1]} />
-							<Lightformer intensity={2} rotation-y={Math.PI / 2} position={[-5, -1, -1]} scale={[10, 2, 1]} />
-							<Lightformer intensity={2} rotation-y={-Math.PI / 2} position={[10, 1, 0]} scale={[20, 2, 1]} />
-							<Lightformer type='ring' intensity={2} rotation-y={Math.PI / 2} position={[-0.1, -1, -5]} scale={10} />
-						</group>
-					</Environment>
+						<Environment resolution={32}>
+							<group rotation={[-Math.PI / 4, -0.3, 0]}>
+								<Lightformer intensity={20} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={[10, 10, 1]} />
+								<Lightformer intensity={2} rotation-y={Math.PI / 2} position={[-5, 1, -1]} scale={[10, 2, 1]} />
+								<Lightformer intensity={2} rotation-y={Math.PI / 2} position={[-5, -1, -1]} scale={[10, 2, 1]} />
+								<Lightformer intensity={2} rotation-y={-Math.PI / 2} position={[10, 1, 0]} scale={[20, 2, 1]} />
+								<Lightformer type='ring' intensity={2} rotation-y={Math.PI / 2} position={[-0.1, -1, -5]} scale={10} />
+							</group>
+						</Environment>
 
-					<ambientLight intensity={0.75} />
-					{/* <Perf /> */}
+						<ambientLight intensity={0.75} />
+						{/* <Perf /> */}
+					</Suspense>
 				</GlobalCanvas>
+				<Loader
+					containerStyles={{ backgroundColor: '#FBFBFD' }}
+					innerStyles={{ backgroundColor: '#FBFBFD' }}
+					dataStyles={{ color: '#4864F3', fontWeight: 'normal', fontSize: '2rem' }}
+					barStyles={{ backgroundColor: '#4864F3' }}
+					dataInterpolation={(p) => `${p.toFixed(2)}`}
+				/>
+
 				<Component {...pageProps} />
 			</Layout>
 		</>
